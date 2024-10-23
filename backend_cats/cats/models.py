baa1eef2831661aae586232bd4ed321dfe095596
GeorgyHub <georgy.polkanov@yandex.ru>
2024-10-23 10:06:21 +0300

    Added model

--- backend_cats/cats/models.py
+++ backend_cats/cats/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Cats(models.Model):
+    name = models.CharField(max_length=50, verbose_name = 'Имя')
+    age = models.PositiveIntegerField(verbose_name = 'Имя')
+    breed = models.CharField(max_length=50, verbose_name = 'Имя')
+    hairiness = models.CharField(max_length=50, verbose_name = 'Имя')
+
+    def __str__(self):
+        return self.name
