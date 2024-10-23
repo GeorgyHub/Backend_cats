cd22fea4d7da674e470f57955d7067de124507be
GeorgyHub <georgy.polkanov@yandex.ru>
2024-10-23 11:34:57 +0300

    Edit models

--- backend_cats/cats/models.py
+++ backend_cats/cats/models.py
@@ -2,10 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Cats(models.Model):
+    class Meta:
+        verbose_name = 'Кот'
+        verbose_name_plural = 'Коты'
+        ordering = ['-age']
+
     name = models.CharField(max_length=50, verbose_name = 'Имя')
-    age = models.PositiveIntegerField(verbose_name = 'Имя')
-    breed = models.CharField(max_length=50, verbose_name = 'Имя')
-    hairiness = models.CharField(max_length=50, verbose_name = 'Имя')
+    age = models.PositiveIntegerField(verbose_name = 'Возраст')
+    breed = models.CharField(max_length=50, verbose_name = 'Порода')
+    hairiness = models.CharField(max_length=50, verbose_name = 'Волосатость')
+    image = models.ImageField(upload_to='media/cats', verbose_name = 'Фото')
 
     def __str__(self):
         return self.name
